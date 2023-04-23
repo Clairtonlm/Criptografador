e47d64c22a0ef6e8404b2075f720178e41feaa47
Clairtonlm <clairtonlm@gmail.com>
2023-04-23 17:06:09 -0300

    add

--- Desafio1/Desafio_Versao_02/script.js
+++ Desafio1/Desafio_Versao_02/script.js
@@ -27,3 +27,11 @@ function changeText(){
     copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value);
   } 
+  function copy() {
+    let copyText = document.querySelector("#input");
+    copyText.select();
+    document.execCommand("copy");
+  }
+  
+  document.querySelector("#copy").addEventListener("click", copy);
+  
